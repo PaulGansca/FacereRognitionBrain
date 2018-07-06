e704af82ce95eddf939c110b76ad8bbb53e64f16
Gansca Paul Mihai <gansca.paul@gmail.com>
2018-07-06 13:30:22 +0300

    Fix initial state bug, now when users log in state is reset

--- src/App.js
+++ src/App.js
@@ -127,23 +127,25 @@ const particlesOptions = {
   "retina_detect": true
 }
 
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: ''
+    }
+}
+
   class App extends Component {
     constructor() {
       super();
-      this.state = {
-        input: '',
-        imageUrl: '',
-        box: {},
-        route: 'signin',
-        isSignedIn: false,
-        user: {
-          id: '',
-          name: '',
-          email: '',
-          entries: 0,
-          joined: ''
-        }
-      }
+      this.state = initialState;
     }
   
     loadUser = (data) => {
@@ -205,7 +207,7 @@ const particlesOptions = {
   
     onRouteChange = (route) => {
       if (route === 'signout') {
-        this.setState({isSignedIn: false})
+        this.setState(initialState);
       } else if (route === 'home') {
         this.setState({isSignedIn: true})
       }
